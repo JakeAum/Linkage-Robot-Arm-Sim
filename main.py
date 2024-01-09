d47f2d6554dbdfad90c578dbaf85c2a06c3753d6
JakeAum <jacobauman00@gmail.com>
2024-01-08 21:08:52 -0700

    Refactor code to use Joint and Link classes

--- main.py
+++ main.py
@@ -3,110 +3,86 @@
 #############################################
 import pygame
 import math
-import numpy as np
+import pygame
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-
-
-class Linkage: 
-    def __init__(self, length, angle, x1, y1,color):
-        self.length = length
-        self._x1 = x1
-        self._y1 = y1
-        self._angle = angle
+class Joint:
+    def __init__(self, x, y, color, is_fixed=False):
+        self.x = x
+        self.y = y
         self.color = color
+        self.is_fixed = is_fixed
+        self.is_dragging = False
 
-    @property
-    def x2(self):
-        self.getEndPoint()
-        return self._x2
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), 10)
 
-    @property
-    def y2(self):
-        self.getEndPoint()
-        return self._y2
-
-    @property
-    def x1(self):
-        return self._x1
-
-    @x1.setter
-    def x1(self, value):
-        self._x1 = value
-
-    @property
-    def y1(self):
-        return self._y1
-
-    @y1.setter
-    def y1(self, value):
-        self._y1 = value
-
-    @property
-    def angle(self):
-        return self._angle
-
-    @angle.setter
-    def angle(self, value):
-        self._angle = value
-
-    def getEndPoint(self):
-        # convert deg to rad
-        angle_rad = math.radians(self.angle)
-
-        # get the end of the linkage
-        self._x2 = self.x1 + self.length * math.cos(angle_rad)
-        self._y2 = self.y1 + self.length * math.sin(angle_rad)
-
-    def draw(self):
-        self.getEndPoint()
-        # draw the linkage in pygame
-        pygame.draw.line(screen, self.color, (self.x1, self.y1), (self.x2, self.y2), 10)
+    def update_position(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
 
 
-### Create Linkage Objects ###
-a,b = 400,400
-link1 = Linkage(200,45,a,b,"black")
-link2 = Linkage(200,180,a,b,"black")
+class Link: 
+    # This Class takes in two Joint Class Instances, and a specified length that the two points must always maintain
+    def __init__(self, jointA, jointB, length):
+        self.jointA = jointA
+        self.jointB = jointB
+        self.length = length
 
-# Create Linkage Objects
-t_c_x = 200
-t_c_y = 200
-l_ab = 100
-l_ac = 100
-l_bc = 100
+    def draw(self, screen):
+        pygame.draw.line(screen, "black", (self.jointA.x, self.jointA.y), (self.jointB.x, self.jointB.y), 5)
 
-link_t_bc = Linkage(l_bc, , t_c_x, t_c_y,"blue")
+    def updateJointPosition(self):
+        # This function will check if the points are within the specified length, and if not, it will then check to see which point are fixed or floating then calculate the new position of the floating point.
 
-n = 45-90
-link_t_ac = Linkage(l_ac, n, t_c_x, t_c_y,"black")
-
-m = -90-45
-t_b_x = link_t_bc.x2
-t_b_y = link_t_bc.y2
-link_t_ab = Linkage(l_ab, m, t_b_x, t_b_y,"red")
-
-# System of Equations that solves for a triangle given 3 side lenghts
-# a = 100
-# b = 100
-# c = 100
-# A = math.acos((b**2 + c**2 - a**2)/(2*b*c))
-# B = math.acos((a**2 + c**2 - b**2)/(2*a*c))
-# C = math.acos((a**2 + b**2 - c**2)/(2*a*b))
+        # Calculate the distance between the two points
+        distance = math.sqrt((self.jointB.x - self.jointA.x)**2 + (self.jointB.y - self.jointA.y)**2)
+        if self.length != distance:
+            if self.jointA.is_fixed == False and self.jointB.is_fixed == True:
+                # Calculate the new position of jointA
+                self.jointA.x = self.jointB.x - (self.jointB.x - self.jointA.x) / distance * self.length
+                self.jointA.y = self.jointB.y - (self.jointB.y - self.jointA.y) / distance * self.length
+            elif self.jointA.is_fixed == True and self.jointB.is_fixed == False:
+                # Calculate the new position of jointB
+                self.jointB.x = self.jointA.x - (self.jointA.x - self.jointB.x) / distance * self.length
+                self.jointB.y = self.jointA.y - (self.jointA.y - self.jointB.y) / distance * self.length
+            elif self.jointA.is_fixed == False and self.jointB.is_fixed == False:
+                # Calculate the new position of both joints
+                angle = math.atan2(self.jointB.y - self.jointA.y, self.jointB.x - self.jointA.x)
+                new_x = self.jointA.x + math.cos(angle) * self.length
+                new_y = self.jointA.y + math.sin(angle) * self.length
+                self.jointB.update_position(new_x, new_y)
+                self.jointA.update_position(new_x - math.cos(angle) * self.length, new_y - math.sin(angle) * self.length)
+            else:
+                raise Exception("Both joints cannot be fixed")
 
 
 
 def main():
-    a = 400
-    b = 400
-    dragging = False
-    offset_x = 0
-    offset_y = 0
-
+    ### pygame init ###
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
     running = True
+    
+    #Center-Ish Origin Point
+    a = 750
+    b = 650
+
+    joint1 = Joint(a, b, "red", is_fixed=True)
+    joint2 = Joint(a, b-1, "blue", is_fixed=False)
+    joint3 = Joint(a-1, b-1, "green", is_fixed=False)
+   
+    joint_list = [joint1, joint2, joint3]
+
+    link12 = Link(joint1, joint2, 400)
+    link12.updateJointPosition()
+    link13 = Link(joint1, joint3, 200)
+    link13.updateJointPosition()
+    link23 = Link(joint2, joint3, 300)
+    link23.updateJointPosition()
+
+    link_list = [link12, link13, link23]
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -114,34 +90,38 @@ def main():
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:  # Left mouse button
-                    if pygame.Rect(a - 10, b - 10, 20, 20).collidepoint(event.pos):
-                        dragging = True
-                        offset_x = event.pos[0] - a
-                        offset_y = event.pos[1] - b
-           
+                
+                    for joint in joint_list:
+                        if (joint.x - 10 <= event.pos[0] <= joint.x + 10 and joint.y - 10 <= event.pos[1] <= joint.y + 10) and not joint.is_fixed:
+                            joint.is_dragging = True
+
             elif event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:  # Left mouse button
-                    dragging = False
-           
+                    for joint in joint_list:
+                        joint.is_dragging = False
+
+                    for link in link_list:
+                        link.updateJointPosition()
+
             elif event.type == pygame.MOUSEMOTION:
-                if dragging:
-                    a = event.pos[0] - offset_x
-                    b = event.pos[1] - offset_y
+                if joint1.is_dragging or joint2.is_dragging or joint3.is_dragging:
+                    for joint in joint_list:
+                        if joint.is_dragging:
+                            joint.update_position(event.pos[0], event.pos[1])
+
+                    for link in link_list:
+                        link.updateJointPosition()
 
         screen.fill("white")
-        link1.x1 = a
-        link1.y1 = b
-        link1.draw()
-        link2.x1 = link1.x1
-        link2.y1 = link1.y1
-        link2.draw()
+        ((1280, 720))
 
-        # draw the triangle
-        link_t_bc.draw()
-        link_t_ac.draw()
-        link_t_ab.draw()
+        pygame.draw.rect(screen, "black", (650, 600, 200 , 200), 5)
         
-        pygame.draw.circle(screen, "red", (a, b), 10)
+        for joint in joint_list:
+            joint.draw(screen)
+        for link in link_list:
+            link.draw(screen)
+    
         pygame.display.flip()
         clock.tick(60)   
     pygame.quit()
